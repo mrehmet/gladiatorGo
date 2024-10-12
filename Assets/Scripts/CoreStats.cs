404ee7f69843f3adaf13e4999c9d46c8452295f7
Bokai-Bi <bokai.bi0@gmail.com>
2024-10-12 19:04:08 -0400

    Init scripts

---
+++ Assets/Scripts/CoreStats.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreStats : MonoBehaviour
+{
+    public static double dollars;
+    public static double dollarsPerSecond;
+    public static List<int> itemCounts;
+    public static List<int> itemPrices;
+    public static List<int> itemEffectiveness;
+}
+
+public enum Upgrades {
+    1_1,
+    1_2,
+    1_3,
+    1_4,
+    2_1,
+    2_2,
+    2_3,
+    2_4,
+    3_1,
+    3_2,
+    3_3,
+    3_4,
+    4_1,
+    4_2,
+    4_3,
+    4_4,
+    5_1,
+    5_2,
+    5_3,
+    5_4,
+    6_1,
+    6_2,
+    6_3,
+    6_4,
+    7_1,
+    7_2,
+    7_3,
+    7_4,
+    8_1,
+    8_2,
+    8_3,
+    8_4,
+    9_1,
+    9_2,
+    9_3,
+    9_4,
+    10_1,
+    10_2,
+    10_3,
+    10_4
+}
