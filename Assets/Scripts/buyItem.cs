29d3fc72734d0df418496c7ab80d8711a496ce09
Michael Rehmet <mrehmet@Michaels-MacBook-Pro-6.local>
2024-10-12 23:14:16 -0400

    ui changes

--- Assets/Scripts/buyItem.cs
+++ Assets/Scripts/buyItem.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class buyItem : MonoBehaviour
 {
     public int id;
+    TMP_Text[] texts;
     // Start is called before the first frame update
     void Start()
     {
-        
+        texts = GetComponentsInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
@@ -19,5 +21,8 @@ public class buyItem : MonoBehaviour
 
     void OnMouseDown() {
         Debug.Log("Button Clicked " + id);
+        CoreStats.BuyItem(id);
+        texts[0].SetText((CoreStats.itemCounts[id]).ToString());
+        texts[1].SetText((CoreStats.itemPrices[id]).ToString());
     }
 }
